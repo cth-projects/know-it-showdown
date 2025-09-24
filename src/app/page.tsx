16544826f0b03235f49b57ac13c3bcf632685c56
kevinlun-git <kevin.lundkvist1@gmail.com>
2025-09-24 19:23:29 +0200

    fix: remove seeddbleaderboard page, duplicate create game button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,19 @@
 import { HydrateClient } from "@/trpc/server";
 import React from "react";
 import Join from "./_components/join";
-import CreateGameButton from "./_components/createGame";
-
-
-
 
 export default function Home() {
-
-  
   return (
-    
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Know-<span className="text-[hsl(280,100%,70%)]">it</span> Showdown
           </h1>
-          
+
           <Join />
-          <CreateGameButton/>
         </div>
       </main>
     </HydrateClient>
   );
 }
-
-
-
