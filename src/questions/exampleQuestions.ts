2daa78e9354ae3c9175e88e05e13f18207b5ba45
kevinlun-git <kevin.lundkvist1@gmail.com>
2025-10-06 11:02:18 +0200

    feat: src/questions folder

---
+++ src/questions/exampleQuestions.ts
@@ -0,0 +1,16 @@
+import { Game0To100CategoryType } from "@prisma/client";
+
+const sampleQuestions = [
+  {
+    question: "What percentage of the human body is approximately water?",
+    answer: 60,
+    categoryName: Game0To100CategoryType.HEALTH,
+    // source: example.com
+  },
+  {
+    question: "What percentage of the world's population can read and write?",
+    answer: 86,
+    categoryName: Game0To100CategoryType.EDUCATION,
+    // source: example2.com
+  },
+];
